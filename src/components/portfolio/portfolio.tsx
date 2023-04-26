33ca2a6bcded0d7d93671e46c8d203cec6ecc370
João Pedro Caetano <caetanojpo@gmail.com>
2023-04-25 23:23:48 -0300

    adjust in portofolio images and avatar implement.

--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -1,21 +1,46 @@
 import { Flex, Image } from "@chakra-ui/react";
 import { useState } from "react";
 export default function Portfolio() {
-  const [portfolioImage, setPortfolioImage] = useState("/portfolio.png");
+  const [portfolioImage, setPortfolioImage] = useState("/port1.png");
 
   setTimeout(() => {
-    if (portfolioImage === "/portfolio.png") {
-      setPortfolioImage("/portfolio2.png");
-    } else if (portfolioImage === "/portfolio2.png") {
-      setPortfolioImage("/portfolio3.png");
-    } else {
-      setPortfolioImage("/portfolio.png");
-    }
-  }, 2800);
+    portfolioImage == "/port1.png"
+      ? setPortfolioImage("/port2.png")
+      : portfolioImage == "/port2.png"
+      ? setPortfolioImage("/port3.png")
+      : portfolioImage == "/port3.png"
+      ? setPortfolioImage("/port4.png")
+      : portfolioImage == "/port4.png"
+      ? setPortfolioImage("/port5.png")
+      : portfolioImage == "/port5.png"
+      ? setPortfolioImage("/port6.png")
+      : portfolioImage == "/port6.png"
+      ? setPortfolioImage("/port7.png")
+      : portfolioImage == "/port7.png"
+      ? setPortfolioImage("/port8.png")
+      : portfolioImage == "/port8.png"
+      ? setPortfolioImage("/port9.png")
+      : portfolioImage == "/port9.png"
+      ? setPortfolioImage("/port10.png")
+      : portfolioImage == "/port10.png"
+      ? setPortfolioImage("/port11.png")
+      : portfolioImage == "/port11.png"
+      ? setPortfolioImage("/port12.png")
+      : portfolioImage == "/port12.png"
+      ? setPortfolioImage("/port13.png")
+      : setPortfolioImage("/port1.png");
+  }, 1000);
 
   return (
     <>
-      <Flex h="100vh" w="100vw" justifyContent="center" alignItems="center">
+      <Flex
+        flexDir="column"
+        h="100vh"
+        w="100%"
+        justifyContent="center"
+        alignItems="center"
+        bg="#242627"
+      >
         <Image h="100%" w="100%" src={portfolioImage}></Image>
       </Flex>
     </>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,37 @@
-import { Flex } from "@chakra-ui/react";
+import { Center, Flex, Image, Link } from "@chakra-ui/react";
 import Portfolio from "../../components/portfolio/portfolio";
+import "./style.scss";
 
 export default function Home() {
   return (
     <>
-      {" "}
-      <Flex bg={"#242627"} h="100%" w="100%">
+      <Flex h="100%" w="100%" maxW="100vw" flexDirection="column">
         <Portfolio />
+        <Flex h="100vh" bg="#242627">
+          <Flex w="50%">oi</Flex>
+          <Center w="50%">
+            <Flex className="avatar" position="relative">
+              <Link
+                href="https://www.linkedin.com/in/caetanojpo/"
+                target="_blank"
+              >
+                <Image
+                  h="100%"
+                  w="100%"
+                  src={"avatar.png"}
+                  position="absolute"
+                  className="avatar-main"
+                ></Image>
+                <Image
+                  className="avatar-animation"
+                  h="100%"
+                  w="100%"
+                  src={"avatarbg.png"}
+                ></Image>
+              </Link>
+            </Flex>
+          </Center>
+        </Flex>
       </Flex>
     </>
   );
